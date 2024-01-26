ec90ecde35c3f3b682f4528345ec48518952163a
ozieblo <59211762+ozieblo-michal@users.noreply.github.com>
2024-01-26 23:18:05 +0100

    fix double logs

--- src/configure_logger.py
+++ src/configure_logger.py
@@ -6,20 +6,20 @@ def configure_logger(log_path: str) -> logging.Logger:
     logger = logging.getLogger("configure_logger")
 
     logger.setLevel(logging.DEBUG)
-    logger.propagate = False 
+    # logger.propagate = False 
 
     formatter = logging.Formatter(
         "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
     )
 
-    file_handler = logging.FileHandler(log_path)
-    file_handler.setLevel(logging.DEBUG)
-    file_handler.setFormatter(formatter)
+    if not logger.handlers:
+        file_handler = logging.FileHandler(log_path)
+        file_handler.setLevel(logging.DEBUG)
+        file_handler.setFormatter(formatter)
+        logger.addHandler(file_handler)
 
-    # console_handler = logging.StreamHandler()
-    # console_handler.setLevel(logging.DEBUG)
-    # console_handler.setFormatter(formatter)
-
-    logger.addHandler(file_handler)
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.DEBUG)
+    console_handler.setFormatter(formatter)
 
     return logger
