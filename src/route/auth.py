4ee8945225bd65c07a23911fd3f52cd9c1d36a02
ozieblo <59211762+ozieblo-michal@users.noreply.github.com>
2024-01-27 12:47:20 +0100

    disable_user bugfix
    db_user.password instead hashed password

--- src/route/auth.py
+++ src/route/auth.py
@@ -17,47 +17,19 @@ router = APIRouter()
 async def login_for_access_token(
     db: Session = Depends(get_db), form_data: OAuth2PasswordRequestForm = Depends()
 ):
-    """Generate an access token for authenticated users.
 
-    This endpoint handles the user login process. Upon successful authentication, it returns a JWT access token.
-
-    Args:
-        form_data (OAuth2PasswordRequestForm): The form data including the username and password.
-
-    Returns:
-        Token: A JWT token encapsulated within a Token schema.
-    """
     return auth.login_for_access_token(db, form_data)
 
-
+# TODO: add get user by ID
 @router.get("/users/me/", response_model=User)
 async def read_users_me(current_user: User = Depends(auth.get_current_active_user)):
-    """Read and return the current authenticated user's information.
 
-    This endpoint retrieves the information of the user who is currently authenticated using their access token.
-
-    Args:
-        current_user (User): The current authenticated user, obtained through dependency injection.
-
-    Returns:
-        User: The User schema containing the user's information.
-    """
     return current_user
 
 
 @router.get("/users/me/items/")
 async def read_own_items(current_user: User = Depends(auth.get_current_active_user)):
-    """Get a list of items owned by the current authenticated user.
 
-    This endpoint is a demonstration of how to access user-specific data,
-    in this case, returning a list of dummy items owned by the user.
-
-    Args:
-        current_user (User): The current authenticated user, obtained through dependency injection.
-
-    Returns:
-        list: A list of dummy items with an item ID and the owner's username.
-    """
     return [{"item_id": "Foo", "owner": current_user.username}]
 
 
@@ -83,20 +55,19 @@ def create_user(user: User, db: Session = Depends(get_db)):
     return service.create_user(db=db, user=user_in_db)
 
 
+# TODO: may be potentially async
 @router.patch("/users/{username}/disable", response_model=User)
 def disable_user(
     username: str, disable_request: DisableUserRequest, db: Session = Depends(get_db)
-):
+) -> User:
     db_user = auth.get_user(db, username=username)
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
 
-    if not auth.verify_password(disable_request.password, db_user.hashed_password):
+    # db_user.password : hashed password
+    if not auth.verify_password(disable_request.password, db_user.password):
         raise HTTPException(status_code=403, detail="Incorrect password")
 
     db_user.disabled = True
     db.commit()
     return db_user
-
-
-# add get by ID
