d1d4e687bf3e83aa9ca6d61eb27c3b1d10c0b7d6
ozieblo <59211762+ozieblo-michal@users.noreply.github.com>
2024-01-27 13:46:00 +0100

    Models docstrings

--- src/model/models.py
+++ src/model/models.py
@@ -1,22 +1,21 @@
 from sqlalchemy import Column, Integer, String, Boolean
 
-# from sqlalchemy.ext.declarative import declarative_base
-
 from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
 
 
 class Dummy(Base):
-    """Model class representing a 'dummy' table in the database.
+    """
+    Model representing the 'dummy' table in the database.
 
-    This class extends `Base`, making it a model class that SQLAlchemy can map to a table.
-    The table will store data about 'Dummy' objects with attributes id, name, and description.
+    This model is used to store information about 'Dummy' objects, with attributes such as id, name, description, and an optional field.
 
     Attributes:
-        id (int): The primary key, automatically generated, unique identifier for each record.
-        name (str): The name attribute, a string that can be NULL.
-        description (str): The description attribute, a string that can also be NULL.
+        id (int): The unique identifier for each record, serving as the primary key of the table.
+        name (str): The name of the Dummy object. It is a required field and cannot be NULL.
+        description (str): A description of the Dummy object. It is a required field and cannot be NULL.
+        optional_field (str): An additional, optional field that can be NULL.
     """
 
     __tablename__ = "dummy"
@@ -28,6 +27,20 @@ class Dummy(Base):
 
 
 class User(Base):
+    """
+    Model representing the 'users' table in the database.
+
+    This model is used for storing user information, including attributes like username, full name, email, password, and disabled status.
+
+    Attributes:
+        id (int): The unique identifier for each user, serving as the primary key of the table.
+        username (str): The username of the user, which is unique and indexed.
+        full_name (str): The full name of the user. It cannot be NULL.
+        email (str): The email address of the user, which is unique and indexed.
+        password (str): The encrypted password of the user. Ensure this is not exposed in API responses.
+        disabled (bool): A flag indicating whether the user account is disabled. Defaults to False.
+    """
+    
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
