271072ecf1391d76f974944f655cffa9fd86650a
ozieblo <59211762+ozieblo-michal@users.noreply.github.com>
2024-06-11 14:08:58 +0200

    fix vite config

--- src/frontend/my-app/vite.config.js
+++ src/frontend/my-app/vite.config.js
@@ -1,8 +1,31 @@
+// import { defineConfig, loadEnv } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// export default defineConfig(({ mode }) => {
+//   const env = loadEnv(mode, import.meta.env.VITE_MODE || '');
+
+//   return {
+//     plugins: [react()],
+//     build: {
+//       outDir: 'dist',
+//     },
+//     base: "/vitefast/",
+//     server: {
+//       proxy: {
+//         '/dummy': env.VITE_BACKEND_URL || 'http://localhost:80',
+//       },
+//     },
+//   };
+// });
+
+
+
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, import.meta.env.VITE_MODE || '');
+  // Załaduj zmienne środowiskowe na podstawie trybu
+  const env = loadEnv(mode, process.cwd());
 
   return {
     plugins: [react()],
